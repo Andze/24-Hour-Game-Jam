bf884a8e65c267354c099a513fea76c79c9bd4eb
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-11-12 14:01:32 +0000

    Added Player 1 and movement for player one

---
+++ RGB-Game-Jam/Assets/Scripts/Player1_Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1_Move : MonoBehaviour {
+
+    public float moveSpeed = 1;
+
+    private Rigidbody myRigidbody;
+
+    void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+	
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        myRigidbody.AddForce(movement * moveSpeed);
+
+    }
+}
