e7b4bfeefc85105e9a3b9ed7e2a935f7b5d413eb
Liam Wilson <liamryanwilson@hotmail.co.uk>
2016-11-13 02:31:19 +0000

    Added score indicator.

---
+++ RGB-Game-Jam/Assets/Scripts/GameController.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ScoreDisplay))]
+public class GameController : MonoBehaviour
+{
+    public Camera cameraTarget;
+    public int granularity = 100;
+
+    public float rScore { get; private set; }
+    public float gScore { get; private set; }
+    public float bScore { get; private set; }
+    private Vector3 greyCompensation;
+    #region RAW VARIABLES (DANGER ZONE)
+    private int texW = 0, texH = 0;
+    private int checkSpacingX = 0, checkSpacingY = 0;
+    private Vector3 colorRaw;
+    private int arraySize = 0;
+    #endregion
+
+    void Start()
+    {
+        texW = cameraTarget.targetTexture.width;
+        texH = cameraTarget.targetTexture.height;
+
+        checkSpacingX = texW / granularity;
+        checkSpacingY = texH / granularity;
+
+        arraySize = granularity * granularity;
+        colorRaw = Vector3.zero;
+        greyCompensation = Vector3.zero;
+    }
+
+    void Update()
+    {
+        CalculateScores();
+        DisplayScoreHUD();
+    }
+
+    private void CalculateScores()
+    {
+        Texture2D mapT2D = RTtoT2D();
+
+        for (int x = 0; x < granularity; x++)
+        {
+            for (int y = 0; y < granularity; y++)
+            {
+                Color temp = mapT2D.GetPixel(checkSpacingX * x, checkSpacingY * y);
+
+                if (temp.r > temp.g && temp.r > temp.b)
+                    colorRaw.x += 1.0f;
+                else if (temp.g > temp.r && temp.g > temp.b)
+                    colorRaw.y += 1.0f;
+                else if (temp.b > temp.r && temp.b > temp.g)
+                    colorRaw.z += 1.0f;
+            }
+        }
+
+        if (greyCompensation == Vector3.zero)
+        {
+            if (colorRaw.x > 0.0f || colorRaw.y > 0.0f || colorRaw.z > 0.0f)
+                greyCompensation = colorRaw;
+        }
+        else SetScores();
+    }
+
+    private void DisplayScoreHUD()
+    {
+
+    }
+
+    private Texture2D RTtoT2D()
+    {
+        RenderTexture.active = cameraTarget.targetTexture;
+        Texture2D t2D = new Texture2D(texW, texH);
+        t2D.ReadPixels(new Rect(0, 0, texW, texH), 0, 0);
+        t2D.Apply();
+
+        return t2D;
+    }
+
+    private void SetScores()
+    {
+        colorRaw = (colorRaw - greyCompensation) / 100.0f;
+
+        rScore = colorRaw.x;
+        gScore = colorRaw.y;
+        bScore = colorRaw.z;
+
+        colorRaw = Vector3.zero;
+    }
+
+    public float GetTotalScore()
+    {
+        return rScore + gScore + bScore;
+    }
+}
---
+++ RGB-Game-Jam/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreDisplay : MonoBehaviour
+{
+    public Canvas hudTarget;
+    
+    private GameObject[] scoreBars;
+    private Vector2 screen;
+    private GameController gController;
+
+    void Start()
+    {
+        screen = new Vector2(Screen.width, Screen.height);
+
+        Rect barRect = new Rect(0.0f, screen.y, screen.x / 3.0f, 10.0f);
+        Vector2 offset = new Vector2(barRect.width / 2.0f, barRect.height / -2.0f);
+        scoreBars = new GameObject[3];
+        scoreBars[0] = CreateUIPanel("rScore", Color.red, barRect, offset);
+        scoreBars[1] = CreateUIPanel("gScore", Color.green, barRect, offset + new Vector2(barRect.width, 0.0f));
+        scoreBars[2] = CreateUIPanel("bScore", Color.blue, barRect, offset + new Vector2(barRect.width * 2.0f, 0.0f));
+
+        gController = GetComponent<GameController>();
+    }
+
+    void Update()
+    {
+        float totalScore = gController.GetTotalScore();
+        Vector3 scorePercentages = new Vector3(gController.rScore / totalScore,
+            gController.gScore / totalScore, gController.bScore / totalScore);
+
+        if (totalScore != 0.0f)
+        {
+            AdjustUIPanel(scoreBars[0], new Vector2(0.0f, screen.y - 10.0f), screen.x * scorePercentages.x);
+            AdjustUIPanel(scoreBars[1], scoreBars[0], screen.x * scorePercentages.y);
+            AdjustUIPanel(scoreBars[2], scoreBars[1], screen.x * scorePercentages.z);
+        }
+    }
+
+    private GameObject CreateUIPanel(string name, Color color, Rect position, Vector2 offset, Transform parent = null)
+    {
+        GameObject newPanel = new GameObject(name);
+        newPanel.AddComponent<CanvasRenderer>();
+        Image i = newPanel.AddComponent<Image>();
+        i.color = color;
+
+        RectTransform t = newPanel.GetComponent<RectTransform>();
+        t.anchoredPosition = Vector2.zero;
+        t.position = new Vector2(position.x, position.y) + offset;
+        t.sizeDelta = new Vector2(position.width, position.height);
+
+        if (parent != null)
+            newPanel.transform.parent = parent;
+        else newPanel.transform.parent = hudTarget.transform;
+
+        return newPanel;
+    }
+
+    private void AdjustUIPanel(GameObject targetPanel, Vector2 newPosition, float newWidth)
+    {
+        RectTransform rTransform = targetPanel.GetComponent<RectTransform>();
+
+        rTransform.position = newPosition + GetOffset(rTransform.rect);
+        rTransform.sizeDelta = new Vector2(newWidth, rTransform.rect.height);
+    }
+
+    private void AdjustUIPanel(GameObject targetPanel, GameObject anchor, float newWidth)
+    {
+        RectTransform rTransform = targetPanel.GetComponent<RectTransform>();
+        RectTransform aTransform = anchor.GetComponent<RectTransform>();
+
+        rTransform.position = (Vector2)aTransform.position + GetOffset(rTransform.rect, true, false) + GetOffset(aTransform.rect, true, false);
+        rTransform.sizeDelta = new Vector2(newWidth, rTransform.rect.height);
+    }
+
+    private Vector2 GetOffset(Rect r, bool x = true, bool y = true)
+    {
+        return new Vector2(x ? r.width / 2.0f : 0.0f, 
+            y ? r.height / -2.0f : 0.0f);
+    }
+}
