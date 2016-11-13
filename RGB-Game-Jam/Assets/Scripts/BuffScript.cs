3b3a4679a28c1b208537ce42d336265be31caa3e
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-11-13 01:56:38 +0000

    Added Buff script with inheritance for each buff and added speed buff

---
+++ RGB-Game-Jam/Assets/Scripts/BuffScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class BuffScript : MonoBehaviour
+{
+    public float cooldownTime = 30.0f;
+    protected bool active = false;
+
+    void Start()
+    {
+        if (!active)
+            GetComponent<Collider>().isTrigger = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!active && other.gameObject.tag == "Player")
+        {
+            Instance(other.gameObject).Activate(cooldownTime);
+            Destroy(this.gameObject);
+        }
+    }
+
+    protected virtual void Activate(float cooldownTime)
+    {
+        active = true;
+        this.cooldownTime = cooldownTime;
+    }
+
+    void Update()
+    {
+        if (active)
+        {
+            cooldownTime -= Time.deltaTime;
+
+            if (cooldownTime <= 0.0f)
+                Destruct();
+        }
+    }
+
+    protected virtual void Destruct()
+    {
+        DestroyImmediate(this);
+    }
+
+    protected virtual BuffScript Instance(GameObject other)
+    {
+        return other.AddComponent<BuffScript>();
+    }
+}
---
+++ RGB-Game-Jam/Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedBoost : BuffScript
+{
+    public float boostedSpeed = 15.0f;
+    private float originalSpeed = 0.0f;
+    
+    protected override void Activate(float cooldownTime)
+    {
+        base.Activate(cooldownTime);
+
+        originalSpeed = GetComponent<PlayerMove>().moveSpeed;
+        GetComponent<PlayerMove>().moveSpeed = boostedSpeed;
+    }
+    
+    protected override void Destruct()
+    {
+        GetComponent<PlayerMove>().moveSpeed = originalSpeed;
+
+        base.Destruct();
+    }
+
+    protected override BuffScript Instance(GameObject other)
+    {
+        return other.AddComponent<SpeedBoost>();
+    }
+}
